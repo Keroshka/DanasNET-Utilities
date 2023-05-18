0265c39224c258cc2788e2190a36702bacc66491
ikerovec20 <ikerovec20@foi.hr>
2023-05-18 19:01:37 +0200

    Add project files.

---
+++ DanasNET-Utilities/DNUtilities.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanasNET_Utilities
+{
+    public class DNUtilities : Plugin<Config>
+    {
+        public override string Author => "Keroshka";
+        public override Version RequiredExiledVersion => new Version(6, 0, 0);
+        public override Version Version => new Version(1, 0, 0);
+        public override string Name => "DanasNET-Utilities";
+
+        public DNUtilities Instance = new DNUtilities();
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+
+            base.OnDisabled();
+        }
+    }
+}
---
+++ DanasNET-Utilities/EventHandlers.cs
@@ -0,0 +1,37 @@
+﻿using CommandSystem.Commands.RemoteAdmin;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Roles;
+using MapGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DanasNET_Utilities
+{
+    public class EventHandlers
+    {
+        private DNUtilities _plugin;
+
+        public EventHandlers(DNUtilities singleton)
+        {
+            _plugin = singleton;
+        }
+
+        public void OnPlayerSpawn()
+        {
+            var players = Exiled.API.Features.Player.List.Where(peanut => peanut.Role.Type == PlayerRoles.RoleTypeId.Scp173).ToList();
+            var position = Exiled.API.Extensions.SpawnExtensions.GetPosition(Exiled.API.Enums.SpawnLocationType.InsideNukeArmory);
+            if (position != Vector3.zero)
+            {
+                foreach (var player in players)
+                {
+                    player.Teleport(Exiled.API.Extensions.SpawnExtensions.GetPosition(Exiled.API.Enums.SpawnLocationType.InsideNukeArmory));
+                }
+            } 
+        }
+    }
+}
